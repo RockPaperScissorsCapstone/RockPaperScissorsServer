9e8474795f859067ef754c33f421b0a490835be1
nbparrell <nbparrell@gmail.com>
2018-10-16 19:38:03 +0000

    Update DataBaseManager.py
    DataBaseManager

---
+++ DataBaseManager.py
@@ -0,0 +1,20 @@
+import mysql.connector
+import array
+
+def class DBManager:
+    private cnx = ''
+    def __init__(self):
+        try:
+            cnx = mysql.connector.connect(user='DatabaseConnect', password='dbConnect',
+                                  host='127.0.0.1',
+                                  database='sys')
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
+        
+    def CreateAccount(userInfo):
+        
